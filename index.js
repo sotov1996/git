3d2b57887b0091abd6131830fc56dcff9e8cf316
ilya.sotov <ilya.sotov@chemwatch.net>
2023-04-13 16:26:00 +0300

    test3

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+<<<<<<< HEAD
 const test4 = "test2"
+=======
+<<<<<<< HEAD
+const test1 = "test1"
+const test2 = "test2"
+=======
+const test3 = "test2"
+>>>>>>> 6d715c7 (test3)
+>>>>>>> f818701 (test3)
